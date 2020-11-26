b78fb1307f9e75ebf24d20ba2f75cfee08f527e8
aortiz34 <aortiz34@ucol.mx>
2020-11-26 16:15:52 -0600

    Added the second function costoRenta

--- src/main.js
+++ src/main.js
@@ -10,6 +10,20 @@ export default class App {
         return total;
     }
 
+    costoRenta(kilometros){
+        let precio;
+        if (kilometros < 50){
+            precio = kilometros * 3;
+        }else if (kilometros >= 50 && kilometros < 100){
+            precio = kilometros * 5;
+        }else if (kilometros >= 100 && kilometros < 200){
+            precio = kilometros * 6;
+        }else {
+            precio = kilometros * 6.5;
+        }
+        return precio;
+    }
+
 }
 
 let app = new App();
@@ -17,3 +31,11 @@ let app = new App();
 console.log(app.convertir(4000));
 console.log(app.convertir(1999));
 console.log(app.convertir(2000));
+
+console.log(app.costoRenta(44));
+console.log(app.costoRenta(50));
+console.log(app.costoRenta(78));
+console.log(app.costoRenta(100));
+console.log(app.costoRenta(158));
+console.log(app.costoRenta(199));
+console.log(app.costoRenta(200));
