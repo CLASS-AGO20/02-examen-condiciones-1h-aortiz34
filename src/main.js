48169b1faf3d6a0956825de62cb8d615d8a67861
aortiz34 <aortiz34@ucol.mx>
2020-11-26 16:14:14 -0600

    Added the first function convertir

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 export default class App {
 
+    convertir(pesos){
+        let total;
+        if (pesos < 2000){
+            total = pesos / 20.5;
+        }else if (pesos >= 2000){
+            total = pesos / 22.7;
+        }
+        return total;
+    }
+
 }
 
 let app = new App();
+
+console.log(app.convertir(4000));
+console.log(app.convertir(1999));
+console.log(app.convertir(2000));
